1cbf414266cbc7f59d3f31b108b1c2fd265f5da4
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-05-08 08:44:57 -0300

    Criação dos métodos da Calculadora
    Criação dos métodos e teste da calculadora

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum():
+    # given a number a = 3 and a number b = 4
+    a = 3
+    b = 4
+
+    # when we calculate the sum
+    output = methods.sum(a, b)
+    
+    # then the sum should be 7
+    assert output == 7
+
+def test_dif():
+    # given a number a = 3 and a number b = 4
+    a = 3
+    b = 4
+
+    # when we calculate the dif
+    output = methods.dif(a, b)
+    
+    # then the dif should be -1
+    assert output == -1
+
+def test_product():
+    # given a number a = 3 and a number b = 4
+    a = 3
+    b = 4
+
+    # when we calculate the product
+    output = methods.product(a, b)
+    
+    # then the product should be 12
+    assert output == 12
+
+def test_div():
+    # given a number a = 12 and a number b = 4
+    a = 12
+    b = 4
+
+    # when we calculate the div
+    output = methods.div(a, b)
+    
+    # then the div should be 7
+    assert output == 3
